671d5b02c1e24f4c46e43b9f898df121150a5626
R3try0 <81508183+R3try0@users.noreply.github.com>
2022-09-16 13:19:53 +0000

    Update ids_alert.py

--- ids_alert.py
+++ ids_alert.py
@@ -1,15 +1,13 @@
-import os,sys,filecmp
+import os,sys,filecmp,subprocess
 from tkinter import messagebox
 from tkinter import *
 from time import sleep
 
+
+
 warning_1 = 0
 warning_2 = 0
-
-def alert():
-	# Just modify it and give an alert sound of your choise:)	
-	s = os.system("mpg123 /home/adam/eas.mp3 > /dev/null 2>&1 & ")
-	return s 
+""" Now adding smtp server to send a notice to administrator"""
 
 def check_modif():
 	check = filecmp.cmp("/var/log/suricata/fast.log","/var/log/suricata/fast2.log")
@@ -37,18 +35,20 @@ def check_modif():
 			break
 	
 
+def activate_service
+
 
 def message():
 	r = messagebox.askyesno("Intrution Detected !!", "An Intrution has been detected it might be a false alarm. Disable internet ?")
 	if r == 1:
 		os.system("ifconfig wlan0 down && ifconfig lo down && ifconfig eth0 down && killall NetworkManager")
-		os.system("killall -9 mpg123 2>&1 &")
+		os.system("killall -9 mpg123")
 	else:
-		os.system("killall -9 mpg123 2>&1 &")
+		os.system("killall -9 mpg123")
 	main()
 
 def print_intru():
-	alert()
+	s = os.system("mpg123 /home/adam/eas.mp3 > /dev/null 2>&1 & ")
 	if warning_2 == 1 and warning_1 == 1:
 		with open('/var/log/snort/snort.alert.fast') as fp:
 			for line in fp:
@@ -80,11 +80,21 @@ def print_intru():
 
 	try:
 		print(file,"\n\n\n\n",file2)
+
 	except:
 		print(file)
+
 	message()
 
 def main():
+	suricata_service_active = subprocess.Popen(["systemctl","is-active","suricata"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	snort_service_active = subprocess.Popen(["systemctl","is-active","snort"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	if suricata_service_active.stdout.read() ==  "b'inactive\n'" and snort_service_active.stdout.read() == "b'inactive\n'":
+		b = os.system("systemctl start suricata && systemctl start snort")
+	elif snort_service_active.stdout.read() == "b'inactive\n'":
+		b = os.system("systemctl start snort")
+	else:
+		b = os.system("systemctl start suricata")
 	warning_1 = 0
 	warning_2 = 0
 	os.system("cp /var/log/snort/snort.alert.fast /var/log/snort/snort.alert.fast.2")
