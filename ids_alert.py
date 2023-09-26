b9b0392772c5135f31a18024e06aa56b0994aeec
F4Ck Y0U <81508183+R3try0@users.noreply.github.com>
2023-09-26 13:01:25 +0300

    Update ids_alert.py

--- ids_alert.py
+++ ids_alert.py
@@ -7,11 +7,10 @@ from time import sleep
 username = os.getlogin()
 warning_1 = 0
 warning_2 = 0
-""" Now adding smtp server to send a notice to administrator"""
 
 def check_modif():
-	check = filecmp.cmp("/var/log/suricata/fast.log","/var/log/suricata/fast2.log")
-	check2 = filecmp.cmp("/var/log/snort/snort.alert.fast","/var/log/snort/snort.alert.fast.2")
+	check = filecmp.cmp("/var/log/suricata/fast.log","/var/log/suricata/fast2.log")# checking the suricata intrution log
+	check2 = filecmp.cmp("/var/log/snort/snort.alert.fast","/var/log/snort/snort.alert.fast.2")# checking the snort intrution log
 	while True:
 		if check == True and check2 == True:
 			check = filecmp.cmp("/var/log/suricata/fast.log","/var/log/suricata/fast2.log")
@@ -34,8 +33,25 @@ def check_modif():
 			print_intru() 
 			break
 
+def mail(ids,file):
+	smtp_server = ""# put your smtp server (like gmail,outlook...etc)
+	sender = ""#  the email that the script is going to login and send the email
+	sender_password = "" # the password of the sender
+	recipient = "" # the email of the recipient
+	message =  f""" Intrution Detected !!", "An Intrution has been detected it by {ids}\n Log: {file}"""# fill free to change the messege {"Title", "Messege"}
+	try:
+		SSL_context = ssl.create_default_context()
+		with smtplib.SMTP(smtp_server + smtp_server2, 587) as server:
+		    server.starttls(context=SSL_context)
+		    server.login(sender, sender_password)
+		    server.sendmail(sender, recipient, message)
+	except:
+		print("Error: No login credentials OR Failed to connect to the SMTP Server. Check your internet connection")
+		return
+
+
 def message():
-	r = messagebox.askyesno("Intrution Detected !!", "An Intrution has been detected it might be a false alarm. Disable internet ?")
+	r = messagebox.askyesno("Intrution Detected !!", "An Intrution has been detected it might be a false alarm. Disable internet ?")# fill free to change the messege
 	if r == 1:
 		os.system("ifconfig wlan0 down && ifconfig lo down && ifconfig eth0 down && killall NetworkManager")
 		os.system("killall -9 mpg123")
@@ -48,30 +64,26 @@ def print_intru():
 	if warning_2 == 1 and warning_1 == 1:
 		with open('/var/log/snort/snort.alert.fast') as fp:
 			for line in fp:
-				pass
-			file = line
+				file += line + "\n"
 
 		with open("/var/log/suricata/fast.log") as fp2:
-			for line in fp2:
-				pass
-			file2 = line
-
+			for line in fp:
+				file += line + "\n"
+		mail("Snort & Suricata",file)
 		r = messagebox.showwarning("IDS Intrution Log",file + "\n\n\n" + file2)
 
 	elif warning_2 == 1:
 		with open('/var/log/snort/snort.alert.fast') as fp:
 			for line in fp:
-				pass
-			file = line
-
+				file += line + "\n"
+		mail("Snort",file)
 		r = messagebox.showwarning("Snort Intrution Log",file)
 
 	else:
-		with open('/var/log/snort/snort.alert.fast') as fp:
+		with open('/var/log/suricata/fast.log') as fp:
 			for line in fp:
-				pass
-			file = line
-			
+				file += line + "\n"
+		mail("Suricata",file)
 		r = messagebox.showwarning("Suricata Intrution Log",file)	
 
 	try:
